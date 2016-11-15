2bb8078deea652e6e320d8b5ddf2160cd469a6ba
Matija Kolarić <matija@jako.sexy>
2016-11-15 19:56:23 +0100

    kraj predavanja

--- conversion_rates/admin.py
+++ conversion_rates/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import ExchangeRate, ExchangeRateList
 
-# Register your models here.
+
+class ExchangeRateInline(admin.TabularInline):
+    """Administration inline interface for Exchange Rate."""
+
+    model = ExchangeRate
+    extra = 0
+    max_num = 2
+
+
+@admin.register(ExchangeRateList, )
+class ExchangeRateListAdmin(admin.ModelAdmin):
+    """Administration interface for Exchange Rate List."""
+
+    inlines = [ExchangeRateInline]
+    list_display = ('list_number', 'date_published',
+                    'date_start', 'number_of_rates')
+
+    def number_of_rates(self, obj):
+        return obj.exchangerate_set.count()
--- conversion_rates/models.py
+++ conversion_rates/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class ExchangeRateList(models.Model):
+    """Exchange rate list contains daily exchange rates."""
+
+    date_published = models.DateField()
+    date_start = models.DateField()
+    list_number = models.PositiveSmallIntegerField()
+
+
+class ExchangeRate(models.Model):
+    """Exchange rate for a single currency on a date from the list."""
+
+    er_list = models.ForeignKey(ExchangeRateList)
+    currency = models.CharField(max_length=3)
+    value = models.DecimalField(decimal_places=8, max_digits=14)
